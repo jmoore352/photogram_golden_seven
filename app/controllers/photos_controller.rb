9f62f692aef0f50d0b7ec6e110435fc2d09907fa
Justin Moore <jmoore3@chicagobooth.edu>
2015-10-26 13:09:53 -0500

    mid

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,32 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+  end
 
+  def new_form
+
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:caption]
+    p.source = params[:source]
+    p.save
+
+    #render("create_row.html.erb")
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @photo_delete = Photo.find_by({ :id => params[:id]})
+    @photo_delete.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+
+  end
 end
